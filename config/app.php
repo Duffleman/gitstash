91e9fe770c201dd964b219ea25a9e2fc43338370
George Miller <george@duffleman.co.uk>
2016-02-21 03:07:02 +0000

    Fixing CORs and removing CSRF

--- config/app.php
+++ config/app.php
@@ -148,6 +148,9 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Barryvdh\Cors\ServiceProvider::class,
+
+
         /*
          * Application Service Providers...
          */
